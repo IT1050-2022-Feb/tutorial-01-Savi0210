f5422924a926a32ae0c71c552f186a2ed175d1f9
it21282072 Satkurulingam <99562725+it21282072Satkurulingam@users.noreply.github.com>
2022-02-13 12:04:59 +0000

    I have done 1st question

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,17 @@
 #include <stdio.h>
 
 int main() {
+  int i;
+  float mark,sum=0,avg;
+  printf("Input marks of 2 subjects:");
+  for(i=0;i<2;i++)
+  {
+    scanf("%f",&mark);
+    sum=sum+mark;
+  }
+  avg=sum/2;
+  printf("\nAverageMark=%.2f",avg);
+  scanf("%f",&avg);
   
   return 0;
 }

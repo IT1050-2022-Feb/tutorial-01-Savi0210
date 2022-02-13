c8c7d2ab297ea1ca18a12cf76b91e83fad3ec720
it21282072 Satkurulingam <99562725+it21282072Satkurulingam@users.noreply.github.com>
2022-02-13 16:22:01 +0000

    I have done exercise 3

--- Tute04.c
+++ Tute04.c
@@ -6,8 +6,8 @@ Do not change the code given in the main() function when you are implementing yo
 
 #include <stdio.h>
 
-int main() {
-   int no1, no2;
+int main(){
+ int no1, no2;
    printf("Enter a value for no 1 : ");
    scanf("%d", &no1);
    printf("Enter a value for no 2 : ");
@@ -17,3 +17,48 @@ int main() {
    printf("%d ", multiply(no1, no2));
    return 0;
 }
+int minimum (int no1 , int no2){
+      
+      int min ;
+
+    if(no1 > no2){
+
+      min = no2 ;
+
+      return min ;
+    }
+    else{
+      min = no1 ;
+
+      return min ;
+    }
+}
+int maximum (int no1 , int no2){
+ int max ;
+   if(no1 > no2 ){
+      
+      max = no1 ;
+
+      return max ;
+   }
+    else{
+       
+       max = no2 ;
+
+       return max ;
+
+    }
+
+}
+int multiply (int no1 , int no2){
+ int mult ;
+
+   mult = no1 * no2 ;
+
+
+   return mult ;
+}
+
+
+
+
